c4c248d375cbb43c04b3bc9f43bad893711f7b06
Pedro Cardona <1724279+atoko@users.noreply.github.com>
2024-12-20 02:50:56 -0800

    nx: Update workspace, bump ~/paloma ~/fourtwo. Include ~/fourtwo-pulumi dependency and create iac/stacks package.

--- iac/workflows/github/release/GithubReleaseReleased.tsx
+++ iac/workflows/github/release/GithubReleaseReleased.tsx
@@ -3,9 +3,8 @@
 
 import {
 	GithubJobBuilder,
-	GithubNodePipelinePackageSteps,
+	GithubWorkflowExpressions,
 } from "@levicape/fourtwo/ci/cd/pipeline/github";
-import { GithubWorkflowExpressions } from "@levicape/fourtwo/ci/cd/pipeline/github";
 import {
 	GithubJobX,
 	GithubStepCheckoutX,
@@ -19,85 +18,130 @@ import {
 } from "@levicape/fourtwo/x/github/node";
 import { NodeGhaConfiguration } from "../push/GithubMainPush.js";
 
-const {
-	current: { register, context: _$_, env },
-} = GithubWorkflowExpressions;
+type CompileAndPublishProps = {
+	cwd?: string;
+	packageName: string;
+};
 
-// const enquirer = new Enquirer();
-// const prompt = enquirer.prompt.bind(enquirer);
+export default (
+	(props: {
+		compileAndPublish?: CompileAndPublishProps[];
+	}) =>
+	async () => {
+		const {
+			current: { register, context: _$_, env },
+		} = GithubWorkflowExpressions;
 
-export default async () => (
-	<GithubWorkflowX
-		name="on Release: [released] Publish @levicape Github"
-		on={{
-			release: {
-				types: ["released"],
-			},
-		}}
-		env={{
-			...register("NPM_REGISTRY_PROTOCOL", "https"),
-			...register("NPM_REGISTRY_HOST", "npm.pkg.github.com"),
-		}}
-	>
-		<GithubJobX
-			id="packages"
-			name="Compile package, test and publish to npm"
-			runsOn={GithubJobBuilder.defaultRunsOn()}
-			contents={"read"}
-			packages={"write"}
-			steps={
-				<>
-					<GithubStepX
-						name={"Verify registry URL"}
-						continueOnError={true}
-						run={[
-							`echo "NPM_REGISTRY_URL: ${env("NPM_REGISTRY_PROTOCOL")}://${env("NPM_REGISTRY_HOST")}"`,
-							`curl -v --insecure ${env("NPM_REGISTRY_PROTOCOL")}://${env("NPM_REGISTRY_HOST")}`,
-						]}
-					/>
-					<GithubStepCheckoutX />
-					<GithubStepNodeSetupX configuration={NodeGhaConfiguration({ env })}>
-						{(node) => {
-							return (
-								<>
-									<GithubStepNodeInstallX {...node} />
-									<GithubStepX
-										name={"Compile module"}
-										run={[
-											new GithubNodePipelinePackageSteps()
-												.getScript(node.configuration)("compile")
-												.build().run as string,
-										]}
-									/>
-									<GithubStepNodeScriptsX {...node} scripts={["lint"]} />
-									<GithubStepNodeScriptsX {...node} scripts={["test"]} />
-									<GithubStepX
-										name={"Increment version"}
-										run={[
-											"export PREID=$RELEVANT_SHA",
-											"export PREID=${PREID:0:10}",
-											`export ARGS="--git-tag-version=false --commit-hooks=false"`,
-											`npm version ${_$_("github.event.release.tag_name")}-$PREID.${_$_("github.run_number")} $ARGS --allow-same-version`,
-										]}
-										env={{
-											RELEVANT_SHA: _$_(
-												"github.event.release.target_commitish || github.sha",
-											),
-										}}
-									/>
-									<GithubStepNodeScriptsX {...node} scripts={["prepublish"]} />
-									<GithubStepX
-										if={"success()"}
-										name={"Increment version"}
-										continueOnError={true}
-										run={["pnpm publish --no-git-checks;"]}
-									/>
-								</>
-							);
-						}}
-					</GithubStepNodeSetupX>
-				</>
-			}
-		/>
-	</GithubWorkflowX>
-);
+		const CompileAndPublish = ({
+			cwd,
+			packageName,
+		}: CompileAndPublishProps) => {
+			const shortname = packageName.split("/").pop();
+			return (
+				<GithubJobX
+					id={`publish_${shortname}`}
+					name={`${packageName}: Compile and publish to Github`}
+					runsOn={GithubJobBuilder.defaultRunsOn()}
+					packages={"write"}
+					contents={"read"}
+					defaults={
+						cwd
+							? {
+									run: { "working-directory": cwd },
+								}
+							: undefined
+					}
+					steps={
+						<>
+							<GithubStepX
+								name={"Verify registry URL"}
+								continueOnError={true}
+								run={[
+									`echo "NPM_REGISTRY_URL: ${env("NPM_REGISTRY_PROTOCOL")}://${env("NPM_REGISTRY_HOST")}"`,
+									`curl -v --insecure ${env("NPM_REGISTRY_PROTOCOL")}://${env("NPM_REGISTRY_HOST")}`,
+								]}
+							/>
+							<GithubStepCheckoutX />
+							<GithubStepNodeSetupX
+								configuration={NodeGhaConfiguration({ env })}
+								options={{}}
+							>
+								{(node) => {
+									return (
+										<>
+											<GithubStepNodeInstallX {...node} />
+											<GithubStepNodeScriptsX {...node} scripts={["compile"]} />
+											<GithubStepNodeScriptsX {...node} scripts={["lint"]} />
+											<GithubStepNodeScriptsX {...node} scripts={["test"]} />
+										</>
+									);
+								}}
+							</GithubStepNodeSetupX>
+							<GithubStepNodeScriptsX
+								configuration={NodeGhaConfiguration({ env })}
+								scripts={["prepublish"]}
+							/>
+							<GithubStepX
+								name={"Increment version"}
+								run={[
+									"export PREID=$RELEVANT_SHA",
+									"export PREID=${PREID:0:10}",
+									`export ARGS="--git-tag-version=false --commit-hooks=false"`,
+									`npm version ${_$_("github.event.release.tag_name")}-$PREID.${_$_("github.run_number")} $ARGS --allow-same-version`,
+								]}
+								env={{
+									RELEVANT_SHA: _$_(
+										"github.event.release.target_commitish || github.sha",
+									),
+								}}
+							/>
+							<GithubStepX
+								if={"success()"}
+								name={"Publish to npm"}
+								continueOnError={true}
+								run={["pnpm publish --no-git-checks;"]}
+							/>
+						</>
+					}
+				/>
+			);
+		};
+
+		const packageScope =
+			props.compileAndPublish?.[0]?.packageName.split("/")[0];
+		const names = props.compileAndPublish
+			?.map((props) => props.packageName.split("/").pop())
+			.join(", ");
+
+		return (
+			<GithubWorkflowX
+				name={`on Release: [released] Publish ${packageScope}/(${names}) to Github`}
+				on={{
+					release: {
+						types: ["released"],
+					},
+				}}
+				env={{
+					...register("NPM_REGISTRY_PROTOCOL", "https"),
+					...register("NPM_REGISTRY_HOST", "npm.pkg.github.com"),
+				}}
+			>
+				{props.compileAndPublish
+					? props.compileAndPublish.map((props) => (
+							<CompileAndPublish {...props} />
+						))
+					: []}
+			</GithubWorkflowX>
+		);
+	}
+)({
+	compileAndPublish: [
+		{
+			packageName: "@levicape/spork",
+		},
+		{
+			packageName: "@levicape/protobuf-spork-stands",
+			cwd: "protocols/stands",
+		},
+	],
+});
